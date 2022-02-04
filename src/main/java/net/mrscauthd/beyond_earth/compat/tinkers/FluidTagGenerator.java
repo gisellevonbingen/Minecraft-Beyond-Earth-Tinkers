66dc707d813904d396aea2f44daadf6f85dd7b0b
gisellevonbingen <gisellevonbingen@gmail.com>
2022-02-04 17:40:45 +0900

    add element optional

--- src/main/java/net/mrscauthd/beyond_earth/compat/tinkers/FluidTagGenerator.java
+++ src/main/java/net/mrscauthd/beyond_earth/compat/tinkers/FluidTagGenerator.java
@@ -2,6 +2,7 @@ package net.mrscauthd.beyond_earth.compat.tinkers;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.FluidTagsProvider;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.mrscauthd.beyond_earth.BeyondEarthMod;
 import slimeknights.mantle.registration.object.FluidObject;
@@ -22,17 +23,19 @@ public class FluidTagGenerator extends FluidTagsProvider
 		this.tagAll(TinkersBeyondEarthFluids.MOLTEN_OSTRUM);
 		this.tagAll(TinkersBeyondEarthFluids.MOLTEN_CALORITE);
 
-		this.tag(TinkerTags.Fluids.METAL_TOOLTIPS).addTags(
-				TinkersBeyondEarthFluids.MOLTEN_DESH.getForgeTag(),
-				TinkersBeyondEarthFluids.MOLTEN_OSTRUM.getForgeTag(),
-				TinkersBeyondEarthFluids.MOLTEN_CALORITE.getForgeTag()
-				);
+		this.tag(TinkerTags.Fluids.METAL_TOOLTIPS).addTags( //
+				TinkersBeyondEarthFluids.MOLTEN_DESH.getForgeTag(), //
+				TinkersBeyondEarthFluids.MOLTEN_OSTRUM.getForgeTag(), //
+				TinkersBeyondEarthFluids.MOLTEN_CALORITE.getForgeTag() //
+		);
 	}
 
 	/** Tags this fluid using local tags */
 	private void tagLocal(FluidObject<?> fluid)
 	{
-		this.tag(fluid.getLocalTag()).add(fluid.getStill(), fluid.getFlowing());
+		TagAppender<Fluid> appender = this.tag(fluid.getLocalTag());
+		appender.addOptional(fluid.getStill().getRegistryName());
+		appender.addOptional(fluid.getFlowing().getRegistryName());
 	}
 
 	/** Tags this fluid with local and forge tags */
